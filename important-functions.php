9c79fb1b67277f187a882505c8980fc56a3f1caa
Muzaffar Shaikh <shaikhmuzffar82@gmail.com>
2025-02-27 14:08:20 +0530

    Add files via upload

---
+++ important-functions.php
@@ -0,0 +1,26 @@
+<?php
+
+$str = "Muzzu";
+// echo strlen($str); //to check length of string
+
+$var = 10;
+// echo var_dump($var); //to check type of variable
+
+
+// echo date("d"); //print current date
+
+// echo is_string($var) //check is string or not
+// echo is_int($var) //check is integer or not
+
+// echo rand(); //generate random values
+
+// die; //stop the code exicution
+
+
+// exit() //stop the code exicution
+
+
+echo substr($str, 3); //generate random values
+
+
+?>
