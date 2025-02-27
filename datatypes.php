a076b30c4ce480191a3634d3123d4e5a941c0198
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-02-27 08:11:05 +0530

    datatypes in php

---
+++ datatypes.php
@@ -0,0 +1,32 @@
+<?php
+
+$string = "this is string";
+echo var_dump($string);
+
+echo "<br/>";
+
+
+$number =10;
+echo var_dump($number);
+
+echo "<br/>";
+
+$decimal = 10.20;
+echo var_dump($decimal);
+
+echo "<br/>";
+
+$bool = true;
+echo var_dump($bool);
+
+echo "<br/>";
+
+$empty = NULL;
+echo var_dump($empty);
+
+echo "<br/>";
+
+$arr =[10, "hello", 20.20];
+echo var_dump($arr);
+
+?>
