194fffe369d11e38700bacaa41d88fe1f751ed08
Muzaffar Shaikh <shaikhmuzffar82@gmail.com>
2025-02-27 12:38:24 +0530

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+    function Hello(){
+        echo "Hello";
+        echo "<br/>";
+        echo "<hr/>";
+    }
+
+    Hello();
+    Hello();
+    Hello();
+    Hello();
+    Hello();
+
+?>
